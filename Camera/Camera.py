525077503498dc19c06aac34ea0cc361afb13238
Ramses Jacquet <jacquetramses38@gmail.com>
2021-02-19 15:54:03 +0100

    Toevoeging  parralel proceccing voorbeelden

--- Camera/Camera.py
+++ Camera/Camera.py
@@ -4,4 +4,19 @@ Created on Fri Feb 19 13:22:00 2021
 
 @author: Ramses
 """
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+print("ik ben iets aan het doen")
+
+while True:
+    _, frame = cap.read()
+
+    cv2.imshow("Ik ben ook maar een persoon",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
 
