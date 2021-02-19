02a7649e9a6db9c9c9eb9ff8a55aba41b385cf1c
SamClercky <sam.clercky@gmail.com>
2021-02-19 10:17:42 +0100

    deleted all generated files from registry

--- initial-project/build/default/production/PI.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/production/PI.p1:  \
-PI.c  \
-mcc_generated_files/mcc.h  \
-mcc_generated_files/device_config.h  \
-mcc_generated_files/pin_manager.h  \
-mcc_generated_files/interrupt_manager.h  \
-mcc_generated_files/tmr2.h  \
-mcc_generated_files/eusart.h  \
-PI.h 
