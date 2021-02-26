09de48d42b2aab34f5f53233c184b9d4fa1d7565
SamClercky <sam.clercky@gmail.com>
2021-02-26 16:59:17 +0100

    Final microcontroller code started

---
+++ MicroController.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=MicroController.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MicroController.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=microcontroller.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/microcontroller.x.tar
