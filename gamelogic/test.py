d86e7de13c281acb03272f4f17708b2669b60b36
ahamdi1801 <50054141+ahamdi1801@users.noreply.github.com>
2021-03-07 16:38:15 +0100

    Add files via upload

---
+++ gamelogic/test.py
@@ -0,0 +1,8 @@
+from gamestate import *
+from setscore import *
+dictt = {1 : ["Player 1", 10], 2 : ["Player 2", 100], 3 : ["Player 3", 100]}
+game = GameState()
+score = MyScore()
+array=[100,651321,123,4656]
+
+print(score.scale(array))
