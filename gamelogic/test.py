ff54a31c35b6e6896fbee5b051e08addcbed807d
ahamdi1801 <50054141+ahamdi1801@users.noreply.github.com>
2021-03-12 15:13:57 +0100

    victory sound file

--- gamelogic/test.py
+++ gamelogic/test.py
@@ -2,10 +2,11 @@ from gamestate import *
 from setscore import *
 from pynput.keyboard import Key, Controller
 from pynput.keyboard import Key, Listener
- 
+from playsound import playsound 
 dictt = {1 : ["Player 1", 10], 2 : ["Player 2", 100], 3 : ["Player 3", 100]}
 game = GameState()
 score = MyScore()
 array=[100,651321,651321,4656]
 dictt={1 : ["anass",100]}
 test=[-10,-315]
+playsound(r"C:\Users\Anass Hamdi\OneDrive - Vrije Universiteit Brussel\Documenten\Github\Project-EIT\Project-EIT\gamelogic\play.wav")
