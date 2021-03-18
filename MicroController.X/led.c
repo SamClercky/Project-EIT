6746b3e994fed75e807cfc20308f0256ab0f68eb
SamClercky <sam.clercky@gmail.com>
2021-03-18 22:14:51 +0100

    werkende 4 led systeem

--- MicroController.X/led.c
+++ MicroController.X/led.c
@@ -3,8 +3,8 @@
 #include "led.h"
 
 void LED_SetStatus(uint8_t flag) {
-	LED1_LAT = flag & 0b0001;
-	LED2_LAT = flag & 0b0010;
-	LED3_LAT = flag & 0b0100;
-	LED4_LAT = flag & 0b1000;
+	LED1_LAT = (flag >> 0) & 1;
+	LED2_LAT = (flag >> 1) & 1;
+	LED3_LAT = (flag >> 2) & 1;
+	LED4_LAT = (flag >> 3) & 1;
 }
